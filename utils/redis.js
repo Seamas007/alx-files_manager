6f647522edad5612d1ecdbc0470f6b17c0959add
Seamas007 <olufy007@gmail.com>
2023-09-01 13:06:17 +0100

    My commit

---
+++ utils/redis.js
@@ -0,0 +1,56 @@
+import redis from 'redis';
+
+class RedisClient {
+  constructor() {
+    this.client = redis.createClient();
+
+    // Handle Redis client errors
+    this.client.on('error', (error) => {
+      console.error('Redis client error:', error);
+    });
+  }
+
+  isAlive() {
+    return this.client.connected;
+  }
+
+  async get(key) {
+    return new Promise((resolve, reject) => {
+      this.client.get(key, (error, value) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(value);
+        }
+      });
+    });
+  }
+
+  async set(key, value, durationInSeconds) {
+    return new Promise((resolve, reject) => {
+      this.client.setex(key, durationInSeconds, value, (error) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve('OK');
+        }
+      });
+    });
+  }
+
+  async del(key) {
+    return new Promise((resolve, reject) => {
+      this.client.del(key, (error) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve('OK');
+        }
+      });
+    });
+  }
+}
+
+const redisClient = new RedisClient();
+
+export default redisClient;
